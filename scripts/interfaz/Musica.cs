c42b6489090f96c67c94e0a47151900c90e0d7b9
Pepesanchez2 <txukyjunior@gmail.com>
2026-04-30 19:47:59 +0200

    Musica arreglada

--- scripts/interfaz/Musica.cs
+++ scripts/interfaz/Musica.cs
@@ -7,14 +7,20 @@ public partial class Musica : Node
 
     public override void _Ready()
     {
-        ProcessMode = ProcessModeEnum.Always; // 🔥 importante (no se pausa)
+        ProcessMode = ProcessModeEnum.Always;
 
         musicPlayer = new AudioStreamPlayer();
         AddChild(musicPlayer);
 
-        var music = GD.Load<AudioStream>("res://assets/musica/musica.mp3"); // tu ruta
+        var music = GD.Load<AudioStream>("res://assets/musica/musica.mp3");
+
+        if (music is AudioStreamMP3 mp3)
+        {
+            mp3.Loop = true;
+        }
+
         musicPlayer.Stream = music;
-        musicPlayer.VolumeDb = -10; // opcional
+        musicPlayer.VolumeDb = -10;
         musicPlayer.Autoplay = true;
 
         musicPlayer.Play();
